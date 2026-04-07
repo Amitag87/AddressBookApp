3fe1b660e84ccdc161c44228be94641e5ddadfd2
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2026-04-07 11:08:06 +0530

    [Amit Agarwal]Refactor.rearranged project structure

--- src/test/java/com/example/demo/AddressBookApplicationTests.java
+++ src/test/java/com/example/demo/AddressBookApplicationTests.java
@@ -1,10 +1,10 @@
-package com.blabz.AddressBookApp;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AddressBookAppApplicationTests {
+class AddressBookApplicationTests {
 
 	@Test
 	void contextLoads() {
